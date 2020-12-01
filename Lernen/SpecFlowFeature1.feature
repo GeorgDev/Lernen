dc8998cdb977c67b211e27b1e69e1eba450bca18
Georg <georg.pretzner@gmail.com>
2020-12-01 19:14:26 +0100

    asd

---
+++ Lernen/SpecFlowFeature1.feature
@@ -0,0 +1,20 @@
+﻿Feature: SpecFlowFeature1
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+@mytag
+Scenario: Sinus von PI
+	Given is pi
+	When Sinus is applied
+	Then the result is 0
+
+Scenario: Cosinus von PI
+	Given is pi
+	When Cosinus is applied
+	Then the result is -1
+
+Scenario: Tangens von PI
+	Given is pi
+	When Tangens is applied
+	Then the result is 0
---
+++ Lernen/SpecFlowFeature1Steps.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace Lernen
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        double number;
+
+        [Given(@"is pi")]
+        public void GivenIsPi()
+        {
+            number = Math.PI;
+        }
+        
+        [When(@"Sinus is applied")]
+        public void WhenSinusIsApplied()
+        {
+            number = Math.Sin(number);
+        }
+        
+        [When(@"Cosinus is applied")]
+        public void WhenCosinusIsApplied()
+        {
+            number = Math.Cos(number);
+        }
+        
+        [When(@"Tangens is applied")]
+        public void WhenTangensIsApplied()
+        {
+            number = Math.Tan(number);
+        }
+        
+        [Then(@"the result is (.*)")]
+        public void ThenTheResultIs(int p0)
+        {
+            double expected = p0;
+            double actual = Math.Round(number);
+            Assert.Equal(expected, actual);
+
+        }
+    }
+}
